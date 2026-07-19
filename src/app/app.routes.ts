734119ae35c80db6bedc078b74a051e74dea01a6
diab0981 <boubacar.siddikh.diallo@usherbrooke.ca>
2026-07-19 22:56:38 +0200

    suppression de ssr et correction le front tourne

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { LoginComponent } from './features/auth/login/login.component';
+
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+
+  { path: '**', redirectTo: 'login' }
+];
