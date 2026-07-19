884ed0fbbc9a31a9397345d3ba303b4f4f1eae90
diab0981 <boubacar.siddikh.diallo@usherbrooke.ca>
2026-07-19 23:29:07 +0200

    ajout de l'ui pour les taches

--- src/app/features/todos/todo-list/todo-list.component.spec.ts
+++ src/app/features/todos/todo-list/todo-list.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
 
 import { TodoListComponent } from './todo-list.component';
 
@@ -8,7 +9,8 @@ describe('TodoListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TodoListComponent]
+      imports: [TodoListComponent],
+      providers: [provideHttpClient()]
     })
     .compileComponents();
 
--- src/app/features/todos/todo-list/todo-list.component.ts
+++ src/app/features/todos/todo-list/todo-list.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatTableModule } from '@angular/material/table';
+import { TodoService } from '../services/todo.service';
 
 @Component({
   selector: 'app-todo-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatCardModule, MatCheckboxModule, MatTableModule],
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.scss'
 })
 export class TodoListComponent {
+  private readonly todoService = inject(TodoService);
+
+  readonly todos$ = this.todoService.getTodos();
+  readonly displayedColumns = ['title', 'completed'];
 
 }
